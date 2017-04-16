7c48adadfb10ff9a81fca18e4f5535e3a24ed29c
Lewis Lancaster <prixelstudios@gmail.com>
2017-04-16 06:51:56 +0100

    Fixed another hard string

--- src/Application/Settings.php
+++ src/Application/Settings.php
@@ -336,6 +336,6 @@ class Settings
 	private static function fileLocation()
 	{
 
-		return sprintf('%s/conf/settings.json', $_SERVER['DOCUMENT_ROOT'] );
+		return sprintf('%s' . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'settings.json', $_SERVER['DOCUMENT_ROOT'] );
 	}
 }
